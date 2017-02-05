0d56f23e38d6a781d0ead215113b32f5a73ad9b3
Sumit Kawatra <kawatrasumit@gmail.com>
2017-02-05 16:39:50 -0500

    Number of digit one initial commit

---
+++ src/com/skawatra/leetcode/NumberOfDigitOne.java
@@ -0,0 +1,23 @@
+package com.skawatra.leetcode;
+
+public class NumberOfDigitOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int countDigitOne(int n) {
+		int result  = -1;
+		int r = -1;
+		while(n>0) {
+			r = n%10;
+			n = n/10;
+			result = 
+		}
+		
+		
+		return result;
+	}
+
+}
