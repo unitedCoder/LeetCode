9f6e42c9223dbcb581452c7d6ddc516ebce33745
Sumit Kawatra <kawatrasumit@gmail.com>
2017-05-09 19:16:47 -0400

    Lame attempts to do leetcode

---
+++ src/com/skawatra/leetcode/FourSum.java
@@ -0,0 +1,24 @@
+package com.skawatra.leetcode;
+
+import java.util.List;
+
+public class FourSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums = {1, 0, -1, 0, -2, 2};
+		int target = 0;
+		
+		List<List<Integer>> solution = fourSum(nums,target);
+		
+		
+	}
+	
+	public static List<List<Integer>> fourSum(int[] nums, int target) {
+		
+		return null;
+        
+    }
+
+}
--- src/com/skawatra/leetcode/NumberOfDigitOne.java
+++ src/com/skawatra/leetcode/NumberOfDigitOne.java
@@ -4,20 +4,54 @@ public class NumberOfDigitOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(countDigitOne(100));
 	}
 	
-	public int countDigitOne(int n) {
-		int result  = -1;
-		int r = -1;
+	public static int countDigitOne(int n) {
+		int count  = 0;
+		int restOfNumber = 0, currentDigit = 0, digitPlace = -1,lastDigit = 0;
 		while(n>0) {
-			r = n%10;
+			
+			currentDigit = n%10;
+			digitPlace++;
+			if(digitPlace > 0)
+				restOfNumber = 10*restOfNumber + lastDigit;
+			
+			// 2 parts to it
+			// calculate number of ones in current digitPlace
+			// add to the count for the last digitPlace
+			
+			if (currentDigit < 1) {
+				if (digitPlace>0) { 
+					count += tenRaisedTo(digitPlace);
+					count += restOfNumber;
+				}
+			} else if (currentDigit == 1) {
+				
+				count += restOfNumber + 1;
+				
+				if(digitPlace > 0)
+					count += currentDigit*(tenRaisedTo(digitPlace-1));
+				
+			} else {
+				count += tenRaisedTo(digitPlace);
+				if(digitPlace > 0)
+					count += currentDigit*(tenRaisedTo(digitPlace-1));
+			}
+			
 			n = n/10;
-			result = 
+			lastDigit = currentDigit;
 		}
 		
-		
-		return result;
+		return count;
+	}
+	
+	public static int tenRaisedTo(int n) {
+		int finalNumber = 1;
+		for(int i=0;i<n;i++) {
+			finalNumber = finalNumber*10;
+		}
+		return finalNumber;
 	}
 
 }
